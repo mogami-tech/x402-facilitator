676ef5f3ce1ce015646f8431cb51409728bae323
straumat <github.backlog426@passmail.net>
2025-06-12 16:28:08 +0200

    /settle tests done

--- src/main/java/tech/mogami/facilitator/web/rest/SettleController.java
+++ src/main/java/tech/mogami/facilitator/web/rest/SettleController.java
@@ -62,12 +62,14 @@ public class SettleController {
             // TODO Make "https://sepolia.base.org" configurable.
             try (Web3j web3j = Web3j.build(new HttpService("https://sepolia.base.org"))) {
 
+                // Loading the contract to use to make the payment.
                 FiatTokenV2_2 contract = FiatTokenV2_2.load(
                         verifyRequest.paymentRequirements().asset(),
                         web3j,
                         new RawTransactionManager(web3j,
                                 Credentials.create(x402Parameters.facilitator().privateKey()),
                                 Long.parseLong(web3j.netVersion().send().getNetVersion())),
+                        // TODO change this to a more suitable gas provider depending on the network chosen.
                         new StaticEIP1559GasProvider(
                                 BASE_SEPOLIA.chainId(),
                                 Convert.toWei("0.002", GWEI).toBigInteger(),   // maxFee ≈ 0.002 gwei
@@ -76,26 +78,51 @@ public class SettleController {
                         )
                 );
 
+                // We create the transaction using the authorization details from the ExactSchemePayload.
                 ExactSchemePayload payload = (ExactSchemePayload) verifyRequest.paymentPayload().payload();
-                var result = contract.transferWithAuthorization(
-                        payload.authorization().from(),
-                        verifyRequest.paymentRequirements().payTo(),
-                        new BigInteger(verifyRequest.paymentRequirements().maxAmountRequired()),
-                        new BigInteger(payload.authorization().validAfter()),
-                        new BigInteger(payload.authorization().validBefore()),
-                        Numeric.hexStringToByteArray(payload.authorization().nonce()),
-                        Numeric.hexStringToByteArray(payload.signature())
-                ).send();
-                System.out.printf("Transaction successful: %s%n", result.getTransactionHash());
+                var transactionReceipt = contract.transferWithAuthorization(
+                                payload.authorization().from(),
+                                verifyRequest.paymentRequirements().payTo(),
+                                new BigInteger(verifyRequest.paymentRequirements().maxAmountRequired()),
+                                new BigInteger(payload.authorization().validAfter()),
+                                new BigInteger(payload.authorization().validBefore()),
+                                Numeric.hexStringToByteArray(payload.authorization().nonce()),
+                                Numeric.hexStringToByteArray(payload.signature()))
+                        .send();
 
+                // We treat the result of the transaction.
+                if (transactionReceipt.isStatusOK()) {
+                    log.info("Successfully settled of request {}: {}",
+                            verifyRequest,
+                            transactionReceipt.getTransactionHash());
+                    return SettleResponse.builder()
+                            .success(true)
+                            .network(verifyRequest.paymentRequirements().network())
+                            .transaction(transactionReceipt.getTransactionHash())
+                            .payer(verifyResult.payer())
+                            .build();
+                } else {
+                    log.error("Failed to settle request {}: {}",
+                            verifyRequest,
+                            transactionReceipt.getStatus());
+                    return SettleResponse.builder()
+                            .success(false)
+                            .network(verifyRequest.paymentRequirements().network())
+                            .errorReason("transaction_failed")
+                            .payer(verifyResult.payer())
+                            .build();
+                }
             } catch (Exception e) {
-                System.out.println("Error getting transaction: " + e.getMessage());
-                return SettleResponse.builder().build();
+                log.error("Exception during request settlement {}: {}",
+                        verifyRequest,
+                        e.getMessage());
+                return SettleResponse.builder()
+                        .success(false)
+                        .network(verifyRequest.paymentRequirements().network())
+                        .errorReason(e.getMessage())
+                        .payer(verifyResult.payer())
+                        .build();
             }
-
-            return SettleResponse.builder()
-
-                    .build();
         }
     }
 
