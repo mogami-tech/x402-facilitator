0c1c41a3b27686fa9dc1c111a6b814525d6cbd67
straumat <stephane.traumat@mogami.tech>
2025-09-29 22:39:20 +0200

    Fix log issue

--- src/main/java/tech/mogami/facilitator/provider/console/ConsoleServiceImplementation.java
+++ src/main/java/tech/mogami/facilitator/provider/console/ConsoleServiceImplementation.java
@@ -8,8 +8,8 @@ import reactor.netty.http.client.HttpClient;
 import tech.mogami.commons.api.console.v1.EventRequest;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static tech.mogami.commons.api.console.ConsoleApiEndpoints.API_BASE_URL;
 import static tech.mogami.commons.api.console.ConsoleApiEndpoints.V1.EVENTS_URL;
-import static tech.mogami.commons.api.console.ConsoleApiEndpoints.V1_PREFIX;
 
 /**
  * {@link ConsoleService} implementation.
@@ -27,26 +27,29 @@ public class ConsoleServiceImplementation implements ConsoleService {
      */
     public ConsoleServiceImplementation() {
         this.client = WebClient.builder()
-                .baseUrl(V1_PREFIX)
+                // TODO Manage development and production URLs
+                .baseUrl(API_BASE_URL)
                 .clientConnector(new ReactorClientHttpConnector(HttpClient.create().followRedirect(true)))
                 .build();
     }
 
     @Override
     public final void logEvent(final EventRequest event) {
-        log.info("{}: Logging payment with nonce {}", event.type(), event.nonce());
-        try {
-            String eventId = client.post()
-                    .uri(EVENTS_URL)
-                    .contentType(APPLICATION_JSON)
-                    .bodyValue(event)
-                    .retrieve()
-                    .bodyToMono(String.class)
-                    .block();
-            log.info("{}: Event logged successfully for event id {}", event.type(), eventId);
-        } catch (Exception e) {
-            log.error("{}: Failed to log event: {}", event.type(), e.getMessage(), e);
-        }
+        client.post()
+                .uri(EVENTS_URL)
+                .contentType(APPLICATION_JSON)
+                .bodyValue(event)
+                .retrieve()
+                .bodyToMono(String.class)
+                .doOnNext(eventId ->
+                        log.info("{}: Event logged successfully for nonce: {} with event id: {}",
+                                event.type(), event.nonce(), eventId)
+                )
+                .doOnError(e ->
+                        log.error("{}: Failed to log event: {}", event.type(), e.getMessage(), e)
+                )
+                .subscribe();
     }
+
 
 }
