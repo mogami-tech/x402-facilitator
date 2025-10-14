cdce768c9c114479a5227e320905df76fc400a40
straumat <stephane.traumat@mogami.tech>
2025-10-14 21:53:09 +0200

    Added log at startup

--- src/main/java/tech/mogami/facilitator/configuration/ParametersConfiguration.java
+++ src/main/java/tech/mogami/facilitator/configuration/ParametersConfiguration.java
@@ -1,15 +1,31 @@
 package tech.mogami.facilitator.configuration;
 
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.web3j.crypto.Credentials;
 import tech.mogami.facilitator.parameter.X402Parameters;
 
 /**
  * Parameter configuration.
  */
+@Slf4j
 @Configuration
 @EnableConfigurationProperties({
         X402Parameters.class
 })
+@RequiredArgsConstructor
 public class ParametersConfiguration {
+
+    /** X402 parameters. */
+    private final X402Parameters x402Parameters;
+
+    @PostConstruct
+    public final void init() {
+        final Credentials credentials = Credentials.create(x402Parameters.facilitator().privateKey());
+        log.info("[X402Parameters] Facilitator address: {}", credentials.getAddress());
+    }
+
 }
--- src/main/java/tech/mogami/facilitator/provider/web3j/GasServiceImplementation.java
+++ src/main/java/tech/mogami/facilitator/provider/web3j/GasServiceImplementation.java
@@ -1,18 +1,19 @@
 package tech.mogami.facilitator.provider.web3j;
 
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.tx.gas.DynamicEIP1559GasProvider;
-import tech.mogami.commons.constant.network.Networks;
 import tech.mogami.commons.crypto.gas.GasFees;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import static tech.mogami.commons.constant.BlockchainConstants.DEFAULT_GAS_FEES;
+import static tech.mogami.commons.constant.network.Networks.ALL_NETWORKS;
 
 /**
  * {@link GasService} implementation.
@@ -28,6 +29,11 @@ public class GasServiceImplementation implements GasService {
     /** Cache of gas fees per network. */
     private final Map<String, GasFees> cache = new ConcurrentHashMap<>();
 
+    @PostConstruct
+    public void init() {
+        ALL_NETWORKS.forEach(network -> log.info("[GasService] {} with rpc {})", network.name(), network.rpcUrl()));
+    }
+
     @Override
     public GasFees getGasFees(final String networkName) {
         return cache.getOrDefault(networkName, DEFAULT_GAS_FEES);
@@ -39,18 +45,17 @@ public class GasServiceImplementation implements GasService {
      */
     @Scheduled(fixedRateString = GAS_FEES_REFRESH_INTERVAL)
     public void refreshAll() {
-        Networks.ALL_NETWORKS
-                .forEach(network -> {
-                    try (Web3j web3j = Web3j.build(new HttpService(network.rpcUrl()))) {
-                        // Getting the latest block to fetch base fee ==================================================
-                        DynamicEIP1559GasProvider provider = new DynamicEIP1559GasProvider(web3j, network.chainId());
-                        GasFees newFees = new GasFees(provider.getMaxFeePerGas(), provider.getMaxPriorityFeePerGas());
-                        cache.put(network.name(), newFees);
-                        log.info("[GasService] Fetched gas fees for network {}: {}", network.name(), newFees);
-                    } catch (Exception e) {
-                        log.error("[GasService] Failed to fetch gas fees for network {}: {}", network.name(), e.getMessage());
-                    }
-                });
+        ALL_NETWORKS.forEach(network -> {
+            try (Web3j web3j = Web3j.build(new HttpService(network.rpcUrl()))) {
+                // Getting the latest block to fetch base fee ==================================================
+                DynamicEIP1559GasProvider provider = new DynamicEIP1559GasProvider(web3j, network.chainId());
+                GasFees newFees = new GasFees(provider.getMaxFeePerGas(), provider.getMaxPriorityFeePerGas());
+                cache.put(network.name(), newFees);
+                log.info("[GasService] Fetched gas fees for network {}: {}", network.name(), newFees);
+            } catch (Exception e) {
+                log.error("[GasService] Failed to fetch gas fees for network {}: {}", network.name(), e.getMessage());
+            }
+        });
     }
 
 }
